4a7fe420f6476a2148f14312f3916ef054ce62aa
eldhojv <eldhojv@gmail.com>
2019-06-04 16:24:24 +0530

    modified main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+#---------------------------------------------------------
+#
+# Author : Eldho
+# Date   : 27/05/2019
+# Project to download OpenImage Dataset version 5
+# Will include segmentation in future release
+#---------------------------------------------------------
+
+import os
+#from modules.csv_downloader import *
+from modules.command_line_parser import *
+from modules.subset_bounding_boxes import *
+
+ROOT_DIR = ''
+DEFAULT_DATASET_DIRECTORY = os.path.join(ROOT_DIR,'OpenImageDataset')
+
+
+if __name__ == "__main__":
+    args = parser_arguments()
+
+if args.tool == 'downloader':
+    subset_bounding_boxes(args, DEFAULT_DATASET_DIRECTORY)
+    # if args.version == 'v5':
+    #     subset_bounding_boxes(args, DEFAULT_DATASET_DIRECTORY)
+    # elif args.version == 'v4':
+    #     print("version v4 not available")
+    #     exit(1)
+    # else:
+    #     print("Enter version v4 or v5")
